01c09e5d32a8d91a1db0c0ad711f9bdbb7e6df9d
Ante Dragoje <antekrtalic2@gmail.com>
2022-05-13 10:58:59 +0200

    Add methods for finding user, deleting, creating and updating

--- create-user/src/main/java/com/user/example/createuser/api/service/UserServiceLayer.java
+++ create-user/src/main/java/com/user/example/createuser/api/service/UserServiceLayer.java
@@ -1,7 +1,16 @@
 package com.user.example.createuser.api.service;
 
+import com.user.example.createuser.dto.UserDTO;
+
+import java.util.List;
+
 public interface UserServiceLayer {
 
     List<UserDTO> findAll();
+    UserDTO getUserById(Long id);
+    void addUser(UserDTO userDTO);
+    UserDTO updateUser(Long id, UserDTO userDTO);
+    UserDTO deleteUser(Long id);
+
 
 }
