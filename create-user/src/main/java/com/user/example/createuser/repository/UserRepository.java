ca28f9ca00927791f933553a6c37c8f8a9162789
Ante Dragoje <antekrtalic2@gmail.com>
2022-05-13 18:44:35 +0200

    Make changes on jdbc mysql driver

--- create-user/src/main/java/com/user/example/createuser/repository/UserRepository.java
+++ create-user/src/main/java/com/user/example/createuser/repository/UserRepository.java
@@ -2,7 +2,8 @@ package com.user.example.createuser.repository;
 
 import com.user.example.createuser.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
