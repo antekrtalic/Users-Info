416dd233196b47b96199a1bf36edf9f95d103d0e
Ante Dragoje <antekrtalic2@gmail.com>
2022-05-13 11:07:24 +0200

    Add model mapper to pom and to UserServiceLayer

---
+++ create-user/src/main/java/com/user/example/createuser/service/UserServiceLayerImpl.java
@@ -0,0 +1,39 @@
+package com.user.example.createuser.service;
+
+import com.user.example.createuser.api.service.UserServiceLayer;
+import com.user.example.createuser.dto.UserDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceLayerImpl extends UserServiceLayer {
+
+    private ModelMapper modelMapper;
+
+    @Override
+    public List<UserDTO> findAll() {
+        return null;
+    }
+
+    @Override
+    public UserDTO getUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void addUser(UserDTO userDTO) {
+
+    }
+
+    @Override
+    public UserDTO updateUser(Long id, UserDTO userDTO) {
+        return null;
+    }
+
+    @Override
+    public UserDTO deleteUser(Long id) {
+        return null;
+    }
+}
