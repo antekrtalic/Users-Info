80a354858430b99e01e5149963881f7d187e894f
Ante Dragoje <antekrtalic2@gmail.com>
2022-08-20 21:31:44 +0200

    Create default user for login and use password encoder for password

--- create-user/src/main/java/com/user/example/createuser/security/ApplicationSecurityConfig.java
+++ create-user/src/main/java/com/user/example/createuser/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.user.example.createuser.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -12,9 +17,23 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/", "index")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails cekiUser = User.builder()
+                .username("ceki")
+                .password("ceki123")
+                .roles("STUDENT") // ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(cekiUser);
     }
 }
