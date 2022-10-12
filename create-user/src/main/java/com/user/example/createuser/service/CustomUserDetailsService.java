2508e869bcb7edd332cf258d8d939c584b558f5e
Ante Dragoje <antekrtalic2@gmail.com>
2022-10-12 22:40:48 +0200

    Add CustomUserDetailsService, implement metho request and add findByUsername function in spring jpa

---
+++ create-user/src/main/java/com/user/example/createuser/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.user.example.createuser.service;
+
+import com.user.example.createuser.entity.User;
+import com.user.example.createuser.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User u = userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("Username not found"));
+
+        return new CustomUserDetails(u);
+    }
+}
