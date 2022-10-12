2c8f1af1bf7ee49f77aff419b15fb0b6ba2f90d8
Ante Dragoje <antekrtalic2@gmail.com>
2022-10-12 22:02:41 +0200

    Create user details and roles entity

---
+++ create-user/src/main/java/com/user/example/createuser/entity/Role.java
@@ -0,0 +1,33 @@
+package com.user.example.createuser.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users = new ArrayList<>();
+
+    public Role(String name) {
+        this.name = name;
+    }
+}
---
+++ create-user/src/main/resources/db/migration/V005__Create_table_user_roles.sql
@@ -0,0 +1,9 @@
+CREATE TABLE user_roles (
+  roles_id BIGINT NOT NULL,
+   users_id BIGINT NOT NULL,
+   CONSTRAINT pk_user_roles PRIMARY KEY (roles_id, users_id)
+);
+
+ALTER TABLE user_roles ADD CONSTRAINT fk_userol_on_role FOREIGN KEY (roles_id) REFERENCES `role` (id);
+
+ALTER TABLE user_roles ADD CONSTRAINT fk_userol_on_user FOREIGN KEY (users_id) REFERENCES user (id);
