7f647530b09af2b022e14f769b9950bcf13eff56
Ante Dragoje <antekrtalic2@gmail.com>
2022-10-21 12:06:43 +0200

    Create tests for methods in taskServiceLayerImpl

--- create-user/src/test/java/com/user/example/createuser/service/UserServiceTests.java
+++ create-user/src/test/java/com/user/example/createuser/service/UserServiceTests.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
@@ -20,7 +19,6 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.*;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTests {
